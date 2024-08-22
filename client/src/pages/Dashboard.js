f3a6fc00844ffbdfb292ea083c78c02bde433d62
root <gitlab_admin_f01a41@example.com>
2024-08-22 03:37:29 +0000

    Added pages

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to the Car Project Manager Dashboard!</h1>
+      {/* Add more content here later */}
+    </div>
+  );
+}
+
+export default Dashboard;
