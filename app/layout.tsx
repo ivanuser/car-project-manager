47cbe6fc5a16b64369adab6007c695c8abff3e57
Ivan Honer <gitlab_admin_f01a41@example.com>
2024-08-28 02:51:56 +0000

    Update 2 files
    - /app/layout.tsx
    - /next.config.js

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>{children}</body>
+    </html>
+  )
+}
